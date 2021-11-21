4f0452f143c90a29f8f59c1cdde57dffd9d07334
Sameer Pasha <spasha@sfu.ca>
2021-11-21 06:34:30 -0800

    Pushing changes to github

---
+++ constants/data.js
@@ -0,0 +1,126 @@
+const userData = {
+  githubUsername: "sampasha",
+  name: "Sameer Pasha",
+  designation: "Data Engineer",
+  avatarUrl: "/avatar.jpg",
+  email: "sampasha007@gmail.com",
+  phone: "+1-(778)-636-1420",
+  address: "127.0.0.1",
+  projects: [
+    {
+      title: "Tailwind Master Kit",
+      link: "https://tailwindmasterkit.com",
+      imgUrl: "/tmk.jpg",
+    },
+    {
+      title: "PlaceholderTech",
+      link: "https://placeholdertech.in",
+      imgUrl: "/placeholdertech.png",
+    },
+    {
+      title: "Portfolio",
+      link: "https://manuarora.in",
+      imgUrl: "/portfolio.png",
+    },
+    {
+      title: "CovidRescue",
+      link: "https://covidrescue.co.in",
+      imgUrl: "/covidrescue.png",
+    },
+    {
+      title: "Algochurn",
+      link: "https://algochurn.com",
+      imgUrl: "/algochurn.png",
+    },
+    {
+      title: "Playground",
+      link: "https://play.placeholdertech.in",
+      imgUrl: "/playground.png",
+    },
+    {
+      title: "Feedmeback",
+      link: "https://feedmeback.vercel.app",
+      imgUrl: "/feedmeback.png",
+    },
+    {
+      title: "Devmedium",
+      link: "https://devmedium.vercel.app",
+      imgUrl: "/devmedium.png",
+    },
+  ],
+  about: {
+    title:
+      "I'm a data engineer that loves building distributed, reliable & highly available data applications on the cloud, that can be scaled as per user needs.",
+    description: [
+      `I'm a passionate engineer with a master's degree and a love for massively scalable systems 🖥️`,
+      `When i'm not behind the computer screen, you can find me in the kitchen experimenting with some recipes 👨🏽‍🍳`,
+      `Anything that can be automated, will be automated 🧰`,
+    ],
+    currentProject: "Data Transfer Service",
+    currentProjectUrl: "",
+  },
+  experience: [
+    {
+      title: "Data Engineer",
+      company: "Article",
+      year: "2021",
+      companyLink: "https://article.com",
+      desc: "Working full time as a data engineer where i am involved with the data services team to build out the internal data platform to cater multiple business needs by processing data at various frequencies and making it highly available.",
+    },
+    {
+      title: "Graduation |  Msc. Computing Science, Specialization in Big Data",
+      company: "Simon  Fraser University",
+      year: "2021",
+      companyLink: "https://sfu.ca",
+      desc: "Masters in Big Data",
+    },
+    {
+      title: "Teaching Assistant",
+      company: "Simon Fraser University",
+      year: "2021",
+      companyLink: "https://www.sfu.ca/students/calendar/2019/summer/courses/cmpt/474.htmlttps://algochurn.com",
+      desc: "Worked as a teaching assistant for the course web systems architecture",
+    },
+    {
+      title: "Research Assistant",
+      company: "Simon Fraser University",
+      year: "2020",
+      companyLink: "https://www.sfu.ca/big-data/services.html",
+      desc: "Worked as a research assistant and acted as a consultant for various project at the SFU big data Hub",
+    },
+    ,
+    {
+      title: "Software Engineer",
+      company: "Huawei Technologies",
+      year: "2017",
+      companyLink: "https://e.huawei.com/en/products/network-management-and-analysis-software",
+      desc: "Worked as a software engineer for the NCE team at huawei that was involved in building the 5G solution for autonomous network management",
+    }
+    ,
+    {
+      title: "Graduation | B.Tech Computer Science",
+      company: "VIT University, Chennai",
+      year: "2017",
+      companyLink: "https://vit.ac.in",
+      desc: "Bachelor's in computer science",
+    },
+    {
+      title: "High School",
+      company: "S.B.O.A School & Junior College",
+      year: "2013",
+      companyLink: "http://www.sboajc.org/",
+      desc: "The place where i completed my schooling",
+    },
+  ],
+  resumeUrl:
+    "https://drive.google.com/file/d/1VhGFv3pV8_ZUlMDb3_qsGprwdY_HQyw2/view?usp=sharing",
+  socialLinks: {
+    instagram: "https://instagram.com/sampasha_007",
+    twitter: "https://twitter.com/sampasha007",
+    linkedin: "https://linkedin.com/in/sameer-pasha",
+    github: "https://github.com/sampasha",
+    facebook: "https://facebook.com/sampasha007",
+  },
+};
+
+export default userData;
