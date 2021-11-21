aa4933c3450c639514b8bebc3fb6e0568b01dea7
Sameer Pasha <spasha@sfu.ca>
2021-11-21 08:01:21 -0800

    Pushing changes to github

--- components/Hero.js
+++ components/Hero.js
@@ -34,8 +34,8 @@ export default function Hero() {
         </RoughNotationGroup>
       </div>
       {/* Image container */}
-      <div className="object-contain lg:block relative w-full md:w-auto sm:w-auto -mr-40 mt-20">
-        <div className="w-auto ">
+      <div className="lg:block relative w-full md:w-1/2 mr-40 mt-20">
+        <div className="w-3/4 ">
           <img src={userData.avatarUrl} alt="avatar" className=" shadow" />
           <div className="flex flex-row justify-between mt-4">
             <div className="flex flex-row space-x-4">
